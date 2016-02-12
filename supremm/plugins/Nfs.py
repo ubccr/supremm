f85a31dd4c43a99191057ab4170c924b35c0ad46
Joseph White <jpwhite4@buffalo.edu>
2016-02-12 16:39:44 -0500

    Add nfs timeseries and summary processors.
    Just includes read and write bytes at the moment.

---
+++ supremm/plugins/Nfs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from supremm.plugin import DeviceBasedPlugin
+
+class Nfs(DeviceBasedPlugin):
+    """ Generate usage statistics for NFS clients """
+
+    name = property(lambda x: "nfs")
+    requiredMetrics = property(lambda x: [
+        "nfsclient.bytes.read.normal",
+        "nfsclient.bytes.read.direct",
+        "nfsclient.bytes.read.server",
+        "nfsclient.bytes.write.normal",
+        "nfsclient.bytes.write.direct",
+        "nfsclient.bytes.write.server"
+        ])
+    optionalMetrics = property(lambda x: [])
+    derivedMetrics = property(lambda x: [])
+
+
