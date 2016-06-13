e9232513f7091c3d7fc99ef3c804fed3bef6687b
Joseph White <jpwhite4@buffalo.edu>
2016-06-13 16:10:44 -0400

    Added timeseries metrics for memory bandwidth, block device and total memory usage.

---
+++ supremm/plugins/BlockTimeseries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+""" Timeseries generator module """
+
+from supremm.plugin import RateConvertingTimeseriesPlugin
+import numpy
+
+class BlockTimeseries(RateConvertingTimeseriesPlugin):
+    """ Generate timeseries summary for block device usage data """
+
+    name = property(lambda x: "block")
+    requiredMetrics = property(lambda x: ["disk.dev.read_bytes",
+                                          "disk.dev.write_bytes"])
+    optionalMetrics = property(lambda x: [])
+    derivedMetrics = property(lambda x: [])
+
+    def __init__(self, job):
+        super(BlockTimeseries, self).__init__(job)
+
+    def computetimepoint(self, data):
+        return numpy.sum(numpy.array(data)) / 1048576.0
