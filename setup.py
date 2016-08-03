2ab585385f3d4f6bfff3a1e7e7a369ef0c8275d0
Joseph White <jpwhite4@buffalo.edu>
2016-08-03 14:13:57 -0400

    Bump version number and update changelog and authors list.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 
 setup(name='supremm',
-      version='1.0.0',
+      version='1.0.1',
       description='SUPReMM Job Summarization Utilities',
       long_description='Utilities for generating job-level summary data from host level PCP archives.\nAlso includes template configuration files for running PCP on an HPC system.',
       license='LGPLv3',
