a091c690f87321ab222cafbcb52b24ae6604c985
Joseph White <jpwhite4@buffalo.edu>
2017-08-01 13:27:33 -0400

    Bump verson

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 
 setup(name='supremm',
-      version='1.0.2',
+      version='1.0.3',
       description='SUPReMM Job Summarization Utilities',
       long_description='Utilities for generating job-level summary data from host level PCP archives.\nAlso includes template configuration files for running PCP on an HPC system.',
       license='LGPLv3',
