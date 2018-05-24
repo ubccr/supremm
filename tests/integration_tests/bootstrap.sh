6643141269ca5af41bb71b6bd8271d9ec73b1ade
Joe White <jpwhite4@buffalo.edu>
2018-05-24 15:58:12 -0400

    Add support for XDMoD 8.0.0 job script schema (#141)

--- tests/integration_tests/bootstrap.sh
+++ tests/integration_tests/bootstrap.sh
@@ -9,10 +9,17 @@ rm -rf /var/lib/mongodb/*
 mongod -f /etc/mongod.conf
 ~/bin/importmongo.sh
 
-mkdir -p /data/{phillips,pozidriv,frearson,mortorq,robertson}/pcp-logs
+mkdir -p /data/{phillips,pozidriv,frearson,mortorq,robertson}/{pcp-logs,jobscripts}
 mkdir -p "/data/mortorq/pcp-logs/hostname/2016/12/30"
 
 python tests/integration_tests/supremm_setup_expect.py
 
 cp tests/integration_tests/pcp_logs_extracted/* /data/mortorq/pcp-logs/hostname/2016/12/30
 
+# Create files containing 'job scripts'
+jspath=/data/phillips/jobscripts/20170101
+mkdir $jspath
+for jobid in 197155 197182 197186 197199
+do
+    echo "Job script for job $jobid" > $jspath/$jobid.savescript
+done
