fa02555cd62aedd5dac0340410b5fe67298919ae
Trey Dockendorf <tdockendorf@osc.edu>
2018-07-26 11:17:10 -0400

    Fix ingest_jobscripts.py to support Torque job arrays

--- src/supremm/ingest_jobscripts.py
+++ src/supremm/ingest_jobscripts.py
@@ -153,7 +153,7 @@ def processfor(resource_id, respath, dbif, timedeltadays):
 
                     dbif.insert({
                         'resource_id': resource_id,
-                        'local_job_id_raw': int(mtch.group(1)),
+                        'local_job_id_raw': mtch.group(1),
                         'start_date': start_date,
                         'script': scriptdata
                     })
