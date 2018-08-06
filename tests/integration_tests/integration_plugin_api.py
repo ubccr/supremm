97dfc551411d5be9d7b39e6b915ed153fd085fc8
Alex Kofke <adkofke@buffalo.edu>
2018-08-06 17:04:10 -0400

    Modify plugin integration test to do multiprocess case as well

--- tests/integration_tests/integration_plugin_api.py
+++ tests/integration_tests/integration_plugin_api.py
@@ -1,13 +1,15 @@
 import sys
 from mock import patch
+import pytest
 
 from supremm import summarize_jobs
 from mock_preprocessor import MockPreprocessor
 from tests.integration_tests.throwing_plugin import InitThrowingPlugin, ProcessThrowingPlugin, ResultsThrowingPlugin
 
 
-def test_plugin_api():
-    test_args = "summarize_jobs.py -d -r 2 -j 972366 --fail-fast".split()
+@pytest.mark.parametrize("threads", [1, 3])
+def test_plugin_api(threads):
+    test_args = "summarize_jobs.py -d -r 2 -j 972366 --fail-fast --threads {}".format(threads).split()
     preprocs = [MockPreprocessor]
     plugins = []
     # this was very non-obvious to me but since summarize_jobs does "from supremm.plugin import loadpreprocs"
@@ -16,24 +18,27 @@ def test_plugin_api():
         summarize_jobs.main()
 
 
-def test_exception_init():
-    test_args = "summarize_jobs.py -d -r 2 -j 972366".split()
+@pytest.mark.parametrize("threads", [1, 3])
+def test_exception_init(threads):
+    test_args = "summarize_jobs.py -d -r 2 -j 972366 --threads {}".format(threads).split()
     plugins = [InitThrowingPlugin]
     preprocs = []
     with patch.object(sys, "argv", test_args), patch("supremm.summarize_jobs.loadpreprocessors",  return_value=preprocs), patch("supremm.summarize_jobs.loadplugins", return_value=plugins):
         summarize_jobs.main()
 
 
-def test_exception_process():
-    test_args = "summarize_jobs.py -d -r 2 -j 972366".split()
+@pytest.mark.parametrize("threads", [1, 3])
+def test_exception_process(threads):
+    test_args = "summarize_jobs.py -d -r 2 -j 972366 --threads {}".format(threads).split()
     plugins = [ProcessThrowingPlugin]
     preprocs = []
     with patch.object(sys, "argv", test_args), patch("supremm.summarize_jobs.loadpreprocessors",  return_value=preprocs), patch("supremm.summarize_jobs.loadplugins", return_value=plugins):
         summarize_jobs.main()
 
 
-def test_exception_results():
-    test_args = "summarize_jobs.py -d -r 2 -j 972366".split()
+@pytest.mark.parametrize("threads", [1, 3])
+def test_exception_results(threads):
+    test_args = "summarize_jobs.py -d -r 2 -j 972366 --threads {}".format(threads).split()
     plugins = [ResultsThrowingPlugin]
     preprocs = []
     with patch.object(sys, "argv", test_args), patch("supremm.summarize_jobs.loadpreprocessors",  return_value=preprocs), patch("supremm.summarize_jobs.loadplugins", return_value=plugins):
