67fe8a498f7adb97c8cc658ab09b2be82e544b30
Alex Kofke <adkofke@buffalo.edu>
2018-08-06 16:55:48 -0400

    Throwing plugin needs to be 3 separate tests
    Since process can skip the rest of the job if it gets an exception,
    not testing the actual broken part.

--- tests/integration_tests/integration_plugin_api.py
+++ tests/integration_tests/integration_plugin_api.py
@@ -3,7 +3,7 @@ from mock import patch
 
 from supremm import summarize_jobs
 from mock_preprocessor import MockPreprocessor
-from tests.integration_tests.throwing_plugin import ThrowingPlugin, InitThrowingPlugin
+from tests.integration_tests.throwing_plugin import InitThrowingPlugin, ProcessThrowingPlugin, ResultsThrowingPlugin
 
 
 def test_plugin_api():
@@ -16,12 +16,25 @@ def test_plugin_api():
         summarize_jobs.main()
 
 
-def test_exception_handling():
+def test_exception_init():
     test_args = "summarize_jobs.py -d -r 2 -j 972366".split()
-    plugins = [InitThrowingPlugin, ThrowingPlugin]
+    plugins = [InitThrowingPlugin]
     preprocs = []
-    # this was very non-obvious to me but since summarize_jobs does "from supremm.plugin import loadpreprocs"
-    # you have to patch loadpreprocs as if it was in the summarize_jobs module
     with patch.object(sys, "argv", test_args), patch("supremm.summarize_jobs.loadpreprocessors",  return_value=preprocs), patch("supremm.summarize_jobs.loadplugins", return_value=plugins):
         summarize_jobs.main()
 
+
+def test_exception_process():
+    test_args = "summarize_jobs.py -d -r 2 -j 972366".split()
+    plugins = [ProcessThrowingPlugin]
+    preprocs = []
+    with patch.object(sys, "argv", test_args), patch("supremm.summarize_jobs.loadpreprocessors",  return_value=preprocs), patch("supremm.summarize_jobs.loadplugins", return_value=plugins):
+        summarize_jobs.main()
+
+
+def test_exception_results():
+    test_args = "summarize_jobs.py -d -r 2 -j 972366".split()
+    plugins = [ResultsThrowingPlugin]
+    preprocs = []
+    with patch.object(sys, "argv", test_args), patch("supremm.summarize_jobs.loadpreprocessors",  return_value=preprocs), patch("supremm.summarize_jobs.loadplugins", return_value=plugins):
+        summarize_jobs.main()
--- tests/integration_tests/throwing_plugin.py
+++ tests/integration_tests/throwing_plugin.py
@@ -19,18 +19,35 @@ class InitThrowingPlugin(Plugin):
         pass
 
 
-class ThrowingPlugin(Plugin):
-    name = property(lambda self: "throwing_plugin")
+class ProcessThrowingPlugin(Plugin):
+    name = property(lambda self: "process_throwing_plugin")
     mode = property(lambda self: "timeseries")
     requiredMetrics = property(lambda self: ["hinv.ncpu", "gpfs.fsios.read_bytes"])
     optionalMetrics = property(lambda self: [])
     derivedMetrics = property(lambda self: [])
 
     def __init__(self, job):
-        super(ThrowingPlugin, self).__init__(job)
+        super(ProcessThrowingPlugin, self).__init__(job)
 
     def process(self, nodemeta, timestamp, data, description):
         raise Exception("Exception in process")
+
+    def results(self):
+        pass
+
+
+class ResultsThrowingPlugin(Plugin):
+    name = property(lambda self: "results_throwing_plugin")
+    mode = property(lambda self: "timeseries")
+    requiredMetrics = property(lambda self: ["hinv.ncpu", "gpfs.fsios.read_bytes"])
+    optionalMetrics = property(lambda self: [])
+    derivedMetrics = property(lambda self: [])
+
+    def __init__(self, job):
+        super(ResultsThrowingPlugin, self).__init__(job)
+
+    def process(self, nodemeta, timestamp, data, description):
+        return False
 
     def results(self):
         raise Exception("Exception in results")
