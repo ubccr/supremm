75af75675afa69066aa5ee8209e532b346cf8e15
Trey Dockendorf <tdockendorf@osc.edu>
2020-10-06 12:49:49 -0400

    Add Infiniband Timeseries for Prometheus

---
+++ src/supremm/plugins/InfiniBandTimeseriesPrometheus.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""https://github.com/prometheus/node_exporter"""
+
+from supremm.plugin import PrometheusTimeseriesNamePlugin
+
+class InfiniBandTimeseriesPrometheus(PrometheusTimeseriesNamePlugin):
+    """ This plugin processes lots of metric that are all interested in the difference over the process """
+
+    name = property(lambda x: "infiniband")
+    metric_system = property(lambda x: "prometheus")
+    requiredMetrics = property(lambda x: {
+        "switch-in-bytes": {
+            'metric': 'rate(node_infiniband_port_data_received_bytes_total{{instance=~"^{node}.+"}}[{rate}])',
+            'timeseries_name': 'receive'
+        },
+        "switch-out-bytes": {
+            'metric': 'rate(node_infiniband_port_data_transmitted_bytes_total{{instance=~"^{node}.+"}}[{rate}])',
+            'timeseries_name': 'transmit'
+        },
+    })
+    optionalMetrics = property(lambda x: {})
+    derivedMetrics = property(lambda x: {})
