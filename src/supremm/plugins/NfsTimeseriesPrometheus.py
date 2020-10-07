3251af0ead6f12d0805383d37f0de41eb66f8369
Trey Dockendorf <tdockendorf@osc.edu>
2020-10-07 09:42:16 -0400

    Add nfs timeseries plugin for Prometheus

---
+++ src/supremm/plugins/NfsTimeseriesPrometheus.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+""" Timeseries generator module """
+
+from supremm.plugin import PrometheusTimeseriesNamePlugin
+
+class NfsTimeseriesPrometheus(PrometheusTimeseriesNamePlugin):
+    """ Generate timeseries summary for NFS usage data """
+
+    name = property(lambda x: "nfs")
+    metric_system = property(lambda x: "prometheus")
+    requiredMetrics = property(lambda x: {
+        "read": {
+            'metric': 'sum(rate(node_mountstats_nfs_total_read_bytes_total{{instance=~"^{node}.+"}}[{rate}]))',
+            'timeseries_name': 'read',
+        },
+        "write": {
+            'metric': 'sum(rate(node_mountstats_nfs_total_write_bytes_total{{instance=~"^{node}.+"}}[{rate}]))',
+            'timeseries_name': 'write',
+        },
+    })
+    optionalMetrics = property(lambda x: {})
+    derivedMetrics = property(lambda x: {})
