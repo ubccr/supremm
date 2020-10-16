631287f5d46d45d9d85feecef5542565049c4576
Joe White <jpwhite4@buffalo.edu>
2020-10-16 11:37:13 -0400

    Temporarily disable the XDMoD stuff

--- tests/integration_tests/integration_test.bash
+++ tests/integration_tests/integration_test.bash
@@ -4,15 +4,17 @@ set -euxo pipefail
 
 indexarchives.py -da
 summarize_jobs.py -d -r 2 -j 972366 --fail-fast
-aggregate_supremm.sh
 
-count=$(mysql -ss -u root <<EOF
-USE modw_supremm;
-SELECT count(*) FROM job WHERE local_job_id=972366 AND resource_id=2 AND netdrv_gpfs_rx IS NOT NULL;
-EOF
-)
-
-[[ $count -eq 1 ]]
+# DISABLED until XDMoD is ported to Centos 8
+#aggregate_supremm.sh
+#
+#count=$(mysql -ss -u root <<EOF
+#USE modw_supremm;
+#SELECT count(*) FROM job WHERE local_job_id=972366 AND resource_id=2 AND netdrv_gpfs_rx IS NOT NULL;
+#EOF
+#)
+#
+#[[ $count -eq 1 ]]
 
 ingest_jobscripts.py
 
