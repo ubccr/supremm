f825af45c87907fffcbfe46e19b74898911aae4c
Conner Saeli <connersa@buffalo.edu>
2022-05-16 17:35:28 +0000

    Forgot to add Debug.py to history

---
+++ src/supremm/plugins/Debug.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Plugin for debugging firstlast plugin behavior in Prometheus summarization code """
+
+from supremm.plugin import Plugin
+
+class Debug(Plugin):
+    """ Various test cases regarding firstlast plugin callbacks can be used here """
+
+    name = property(lambda x: "debug")
+    mode = property(lambda x: "firstlast")
+    requiredMetrics = property(lambda x: ["DEBUG"])
+
+    optionalMetrics = property(lambda x: [])
+
+    derivedMetrics = property(lambda x: [])
+
+    def __init__(self, job):
+        super(Debug, self).__init__(job)
+        self._numcallbacks = 0
+        
+    def process(nodemeta=None, timestamp=None, data=None, description=None):
+        self._numcallbacks += 1
+        
+    def results():
+        return {"callbacks": self._numcallbacks}
