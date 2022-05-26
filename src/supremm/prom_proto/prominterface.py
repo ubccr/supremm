b36d76f8ab0a5eebfd2f6b27b373632082062486
Conner Saeli <connersa@buffalo.edu>
2022-05-26 13:12:05 +0000

    about to work on numpy array allocation

--- src/supremm/prom_proto/prominterface.py
+++ src/supremm/prom_proto/prominterface.py
@@ -1,3 +1,4 @@
+import logging
 import urllib.parse as urlparse
 
 import numpy as np
@@ -28,6 +29,7 @@ class PromClient():
         if r.status_code != 200:
             print(str(r.content))
             return None
+
         data = r.json()
         plugin_data = formatforplugin(data)
         return plugin_data
@@ -48,7 +50,9 @@ class PromClient():
         if r.status_code != 200:
             print(r.content)
             return None
+
         data = r.json()
+
         plugin_data = formatforplugin(data)
         return plugin_data
 
@@ -65,7 +69,6 @@ class PromClient():
         return
 
     def label_val_meta(self, start, end, matches, l):
-        "    def label_val_meta(self, start, end, matches, l):
         """
         Queries label values for a given metric.
         """
@@ -81,7 +84,7 @@ class PromClient():
 
         urlparse.urlencode(params, doseq=True)
         url = urlparse.urljoin(self.url, "/api/v1/label/%s/values" % l)
-#        logging.debug('Prometheus QUERY LABEL VALUES, url="(%s).20s" start=%s end=%s', url, start, end)
+        logging.debug('Prometheus QUERY LABEL VALUES, url="(%s).20s" start=%s end=%s', url, start, end)
 
         # Get data
         r = requests.get(url, params=params, headers=headers)
@@ -92,36 +95,12 @@ class PromClient():
 
         # Format for plugin
         label_idx = np.arange(0, len(data["data"]))
-        return [label_idx, data["data"]]""
-        Queries label values for a given metric.
-        """
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-        params = {
-            'match[]': matches,
-            'start': str(start),
-            'end': str(end)
-        }
-        # type(l) == set, len(l) == 1
-
-        urlparse.urlencode(params, doseq=True)
-        url = urlparse.urljoin(self.url, "/api/v1/label/%s/values" % l)
-#        logging.debug('Prometheus QUERY LABEL VALUES, url="(%s).20s" start=%s end=%s', url, start, end)
-
-        # Get data
-        r = requests.get(url, params=params, headers=headers)
-        if r.status_code != 200:
-            print(r.content)
-            return False
-        data = r.json()
-
-        # Format for plugin
-        label_idx = np.arange(0, len(data["data"]))
-        return [label_idx, data["data"]] 
+        return [label_idx, data["data"]]
 
 def formatforplugin(rdata):
     rtype = rdata["data"]["resultType"]
+    numtimestamps = len(data["data"]["result"][0]["values"]))
+
     # Process vector
     if rtype == "vector":
         return formatvector(rdata["data"]["result"])
@@ -150,6 +129,4 @@ if __name__=="__main__":
     end = "2022-05-02T09:00:00.000Z"
 
     client = PromClient(url)
-    data = client.query_range("node_cpu_seconds_total{mode='user'}", start, end)
-    for item in data:
-        print(item)
+    data = client.query_range("node_cpu_seconds_total{mode='user'} * 1000", start, end)
