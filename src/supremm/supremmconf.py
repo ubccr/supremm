89d0e2c16d616d62b8d40d2cd78802f4de1e04f6
Conner Saeli <connersa@buffalo.edu>
2022-11-30 22:08:29 +0000

    Updates to the configuration check utility.
    supremmconf.py
    1.) Added debug flag
    2.) Added flag to specify configuration path
    3.) Can now print entire configuration
    4.) Better handling for invalid sections or items
    
    config.py
    1.) Added __str__ method for easier printing

--- src/supremm/supremmconf.py
+++ src/supremm/supremmconf.py
@@ -4,28 +4,39 @@
 import sys
 import os
 import json
+import logging
 from getopt import getopt
 
 from supremm.config import Config
+from supremm.scripthelpers import setuplogger
 
 def usage():
     """ print usage """
     print("usage: {0} [OPTS]".format(os.path.basename(__file__)))
+    print("  -d --debug            set log level to debug")
+    print("  -c --config           specify the configuration file")
+    print("  -r --resources        output the configuration data for all enabled resources") 
     print("  -s --section SECTION  output the configuration data from the specified section")
     print("  -i --item ITEM        output the configuration data for the specified item")
     print("  -h --help             print this help message")
 
-
 def getoptions():
     """ process comandline options """
 
-    retdata = {"section": None, "item": None}
+    retdata = {"log"	: logging.ERROR,
+               "config"	: None,
+               "section": None,
+               "item"	: None}
 
-    opts, _ = getopt(sys.argv[1:], "s:i:h", ["section=", "item="])
+    opts, _ = getopt(sys.argv[1:], "dc:s:i:h", ["debug", "config=", "section=", "item="])
 
     for opt in opts:
+        if opt[0] in ("-d", "--debug"):
+            retdata['log'] = logging.DEBUG 
+        if opt[0] in ("-c", "--config"):
+            retdata['config'] = opt[1]
         if opt[0] in ("-s", "--section"):
-            retdata['section'] = opt[1].encode("utf-8")
+            retdata['section'] = opt[1]#.encode("utf-8")
         if opt[0] in ("-i", "--item"):
             retdata['item'] = opt[1]
         if opt[0] in ("-h", "--help"):
@@ -41,18 +52,44 @@ def getoptions():
 def main():
     """ print out config data according to cmdline args """
     opts = getoptions()
-    conf = Config()
-    
+
+    setuplogger(opts['log'])
+
+    if opts['config']:
+        logging.debug("Using specified path: {}".format(opts['config']))
+    else:
+        logging.debug("Automatically detecting configuration path.")
+
+    try:
+        conf = Config(opts['config'])
+    except:
+        logging.error("Configuration could not be found.")
+        sys.exit(1)
+
+    if not opts['section']:
+       print(conf)
+       sys.exit(0)
+
     try:
         section = conf.getsection(opts['section'])
-        if opts['item'] != None:
-            print(section[opts['item']])
-        else:
-            print(json.dumps(section, indent=4))
-
     except KeyError:
-        sys.stderr.write("Error section \"%s\" not defined in configuration file.\n" % (opts['section']))
+        logging.error("Section '{}' not defined in configuration file.".format(opts['section']))
         sys.exit(1)
+
+    if opts['item']:
+        try:
+            item = section[opts['item']]
+        except KeyError:
+            logging.error("Item '{}' not defined in section '{}'.".format(opts['item'], opts['section']))
+            sys.exit(1)
+
+        if isinstance(item, dict):
+            item = json.dumps(item, indent=4)
+ 
+        print("{} : {}".format(opts['item'], item))
+
+    else:
+        print(json.dumps(section, indent=4))
 
 if __name__ == "__main__":
     main()
