feebec997d170cae00e779ab371ebb2281866d98
Conner Saeli <connersa@buffalo.edu>
2023-06-12 11:27:22 -0400

    Add deps for install in test step

--- tests/ci/test/bootstrap.sh
+++ tests/ci/test/bootstrap.sh
@@ -2,6 +2,21 @@
 set -euxo pipefail
 shopt -s extglob
 
+# Install dependencies
+dnf install -y \
+    python36 \
+    python3-pymongo \
+    python3-numpy \
+    python3-scipy \
+    python3-PyMySQL \
+    python3-pcp \
+    python3-pcp \
+    pcp-libs \
+    python3-Cython \
+    python3-pytz \
+    python3-requests
+    python3-pexpect
+
 INSTALL_TYPE=$1
 case $INSTALL_TYPE in
   "rpm")
@@ -11,18 +26,6 @@ case $INSTALL_TYPE in
     pip3 install -y dist/supremm-+([0-9.])*.whl
     ;;
   "src")
-    dnf install -y \
-        python36 \
-        python3-pymongo \
-        python3-numpy \
-        python3-scipy \
-        python3-PyMySQL \
-        python3-pcp \
-        python3-pcp \
-        pcp-libs \
-        python3-Cython \
-        python3-pytz \
-        python3-requests
     python3 setup.py install
     ;;
 esac
@@ -33,7 +36,6 @@ mkdir -p "/data/pcp_cluster/pcp-logs/hostname/2016/12/30"
 mkdir -p "/data/prom_cluster/jobscripts"
 
 # Run setup script
-dnf install -y python3-pexpect
 
 export TERMINFO=/bin/bash
 export TERM=linux
