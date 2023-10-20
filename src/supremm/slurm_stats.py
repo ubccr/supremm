9415af5c051d0421848e3cce8e76d8f7b6df9cf8
Joseph White <jpwhite4@buffalo.edu>
2023-10-20 17:01:59 -0400

    Tify up code
    remove unused functions. Clean up pylint warning messages.

--- src/supremm/slurm_stats.py
+++ src/supremm/slurm_stats.py
@@ -4,12 +4,9 @@
 import argparse
 import json
 import datetime
-import math
 import re
 import gzip
-import sys
 import os
-import pytz
 
 from ClusterShell.NodeSet import NodeSet
 from supremm.config import Config
@@ -18,60 +15,26 @@ from supremm import outputter
 
 RUNLOG_FILE='.slurm_stats.json'
 
-def formatmemory(value):
 
-    unit = 'M'
-
-    if value >= 1024 and value % 512 == 0:
-        unit = 'G'
-        value = value / 1024
-
-    if math.floor(value) == value:
-        return "{}{}".format(int(math.floor(value)), unit)
-
-    return "{:.02f}{}".format(value, unit)
-
-def getmemory(info, _):
-
-    if 'tres' in info and 'allocated' in info['tres']:
-        for record in info['tres']['allocated']:
-            if record['type'] == 'mem':
-                return formatmemory(record['count'])
-
-    if 'tres' in info and 'requested' in info['tres']:
-        for record in info['tres']['requested']:
-            if record['type'] == 'mem':
-                return formatmemory(record['count'])
-
-    return formatmemory(0)
-
-def getstrfield(info, field):
-    return str(getfield(info, field))
 
 def getfield(info, field):
-
+    """ helper function to get a nested field name from an object
+    """
     s = info
     for key in field.split('.'):
         if key in s:
             s = s[key]
         else:
-            return "******** {} *********".format(field)
+            return None
 
     return s
 
-def serializetime(info, field):
-    ts = getfield(info, field)
-    if ts <= 0:
-        return "Unknown"
-
-    dt = datetime.datetime.utcfromtimestamp(ts)
-
-    return dt.astimezone(pytz.timezone("Pacific/Samoa")).strftime("%Y-%m-%dT%H:%M:%S")
-
-def gettresvaluestr(info, rtype, rname = None):
-    return str(gettresvalue(info, rtype, rname))
-
 def gettresvalue(info, rtype, rname = None):
+    """ helper function to get a a TRES value from a slurm json accounting
+        record. This follows a similar algorithm used by sacct. If the job
+        has been allocated resource then the allocated value is used, otherwise
+        the requested value is used. 0 is returned if no data found.
+    """
 
     if 'tres' in info and 'allocated' in info['tres']:
         for record in info['tres']['allocated']:
@@ -87,67 +50,27 @@ def gettresvalue(info, rtype, rname = None):
 
     return 0
 
-def serializetres(info, ttype):
-
-    out = []
-    if 'tres' in info and ttype in info['tres']:
-        for record in info['tres'][ttype]:
-            if record['type'] == 'mem':
-                out.append(record['type'] + "=" + formatmemory(record['count']))
-            elif record['type'] in ['node', 'cpu', 'billing']:
-                out.append(record['type'] + "=" + str(record['count']))
-            elif record['type'] == 'gres':
-                out.append( record['type'] + '/' + record['name'] + "=" + str(record['count']))
-
-    out.sort()
-
-    return ",".join(out)
-
-def getarrayjobid(info, _ = None):
-    if 'array' in info and 'job_id' in info['array'] and info['array']['job_id'] != 0:
-        return str(info['array']['job_id']) + "_" + str(info['array']['task_id'])
-
-    return str(info['job_id'])
-
 def getarrayjobinfo(info, _ = None):
+    """ helper function to get the job_id, job_id_raw and job_array_index from a slurm
+        json accounting record.
+    """
     if 'array' in info and 'job_id' in info['array'] and info['array']['job_id'] != 0:
         return (str(info['array']['job_id']) + "_" + str(info['array']['task_id']), info['array']['job_id'], info['array']['task_id'])
 
     return (str(info['job_id']), str(info['job_id']), -1)
 
 def durationseconds(info, field):
+    """ helper function to get the time in seconds from a slurm minutes field
+    """
     time_minutes = getfield(info, field)
     if time_minutes is None:
         return 0
     return time_minutes * 60
 
-def mintostring(info, field):
-    time_minutes = getfield(info, field)
-    if time_minutes is None:
-        return "Partition_Limit"
-    return timetostr(time_minutes * 60)
-
-def secondstostring(info, field):
-    time_seconds = getfield(info, field)
-    return timetostr(time_seconds)
-
-def timetostr(intime):
-    seconds = intime % 60
-    time = intime // 60
-    minutes = time % 60
-    hours = time // 60 % 24
-    days = time // 1440
-
-    if days < 0 or hours < 0 or minutes < 0 or seconds < 0:
-        return "INVALID"
-
-    if days > 0:
-        return "{}-{:0>2}:{:0>2}:{:0>2}".format(days, hours, minutes, seconds)
-
-    return "{:0>2}:{:0>2}:{:0>2}".format(hours, minutes, seconds)
-
 def starttime_ts(info):
-
+    """ helper function to get the start time. This follows the same algorithm
+        as the slurm sacct code
+    """
     ts = None
     for step in info['steps']:
         ts1 = step['time']['start']
@@ -164,34 +87,9 @@ def starttime_ts(info):
 
     return ts
 
-def starttime(info, _ = None):
-
-    ts = None
-    for step in info['steps']:
-        ts1 = step['time']['start']
-        if ts is None:
-            ts = ts1
-        else:
-            ts = min(ts, ts1)
-
-    if ts is None:
-        if info['time']['elapsed'] == 0:
-            ts = info['time']['end']
-        else:
-            ts = info['time']['start']
-
-    dt = datetime.datetime.utcfromtimestamp(ts)
-
-    return dt.astimezone(pytz.timezone("Pacific/Samoa")).strftime("%Y-%m-%dT%H:%M:%S")
-    #return dt.strftime("%Y-%m-%dT%H:%M:%S")
-
-def getnamefield(info, field):
-    name = getstrfield(info, field)
-
-    return name.strip('“”')
-
 def getexitcode(info, field):
-
+    """ helper function to ge the exit code from a slurm sacct json record
+    """
     state = getfield(info, 'state.current')
     if state == 'FAILED':
         return "1:0"
@@ -205,17 +103,18 @@ def getexitcode(info, field):
         return_code = signal
         signal = 0
 
-    return "{}:{}".format(return_code, signal)
+    return f"{return_code}:{signal}"
 
 def slurm_job_to_supremm(job, resource_id):
-
+    """ generate a supremm job summary record from a slurm job accounting record
+        formt he slurm sacct --json format.
+    """
     if job['nodes'] == 'None assigned':
         return None, None
 
     now = datetime.datetime.now(datetime.timezone.utc)
     job_uniq_id, local_job_id, local_job_array_index = getarrayjobinfo(job)
     ncpus = gettresvalue(job, 'cpu')
-    endtime = getfield(job, 'time.end')
     exit_status =  getfield(job, 'state.current')
 
     if exit_status == 'RUNNING':
@@ -241,7 +140,7 @@ def slurm_job_to_supremm(job, resource_id):
             "local_job_id" : local_job_id,
             "local_job_array_index" : local_job_array_index,
             "group" : getfield(job, 'group'),
-            "reqmem" : getmemory(job, 'allocated'),
+            "reqmem" : gettresvalue(job, 'mem'),
             "eligible" : getfield(job, 'time.eligible'),
             "exit_code" : getexitcode(job, 'derived_exit_code'),
             "submit" : getfield(job, 'time.submission'),
@@ -249,14 +148,14 @@ def slurm_job_to_supremm(job, resource_id):
             "nodes" : gettresvalue(job, 'node'),
             "job_uniq_id" : job_uniq_id,
             "start_time" : starttime_ts(job),
-            "jobname" : getnamefield(job, 'name'),
+            "jobname" : getfield(job, 'name'),
             "user" : getfield(job, 'user'),
             "gpus" : gettresvalue(job, 'gres', 'gpu'),
             "resource_manager" : "slurm",
             "account" : getfield(job, 'account'),
             "partition" : getfield(job, 'partition'),
             "timelimit": durationseconds(job, 'time.limit'),
-            "end_time" : endtime
+            "end_time" : getfield(job, 'time.end')
         },
         "cpu" : {
             "jobcpus" : {
@@ -304,7 +203,8 @@ def slurm_job_to_supremm(job, resource_id):
     return out, mdata
 
 def process_file(entry, config, resconf, dryrun):
-
+    """ process a slurm json file for a configured resource
+    """
     job_count = 0
 
     with gzip.open(entry.path, "r") as jfile:
@@ -319,66 +219,13 @@ def process_file(entry, config, resconf, dryrun):
                 job_count += 1
 
                 if job_count % 500 == 0:
-                    print("Processed {} jobs".format(job_count))
+                    print(f"Processed {job_count} jobs")
 
-
-# jobid,jobidraw,cluster,partition,qos,account,group,gid,user,uid,\
-# submit,eligible,start,end,elapsed,exitcode,state,nnodes,ncpus,reqcpus,reqmem,\
-# reqtres,alloctres,timelimit,nodelist,jobname
-
-def process_file_sacct(filename):
-
-    fields = [
-            ('_', getarrayjobid),
-            ('job_id', getstrfield),
-            ('cluster', getfield),
-            ('partition', getfield),
-            # temp disabled ('qos', getfield),
-            ('account', getfield),
-            ('group', getfield),
-            #('gid', not supported),
-            ('user', getfield),
-            #('uid', not supported),
-            ('time.submission', serializetime),
-            ('time.eligible', serializetime),
-            ('time.start', starttime),
-            ('time.end', serializetime),
-            ('time.elapsed', secondstostring),
-            ('derived_exit_code', getexitcode),
-            ('state.current', getfield),
-            ('node', gettresvaluestr),
-            ('cpu', gettresvaluestr),
-            ('required.CPUs', getstrfield),
-            ('allocated', getmemory),
-            ('requested', serializetres),
-            ('allocated', serializetres),
-            ('time.limit', mintostring),
-            ('nodes', getfield),
-            ('name', getnamefield)
-    ]
-
-    with gzip.open(filename, "r") as jfile:
-        alldata = json.load(jfile)
-        for data in alldata['jobs']:
-            if getstrfield(data, 'state.current') in ['RUNNING', 'PENDING']:
-                continue
-
-            output = []
-            for field, fn in fields:
-                try:
-                    output.append(fn(data, field))
-                except TypeError as e:
-                    print(e)
-                    print(field)
-                    print(data)
-                    return
-
-            print("|".join(output))
-
-def print_help():
-    print("Usage: {} PATH_TO_FILES")
 
 def getrunlog():
+    """ Get the object containing the metadata from the most
+        recent succesful run on the process
+    """
     mlog = {'last_mtime': 0}
 
     try:
@@ -390,11 +237,14 @@ def getrunlog():
     return mlog
 
 def saverunlog(mlog):
+    """ Save to file the object containing the run metadata
+    """
     with open(RUNLOG_FILE, 'w', encoding='utf8') as fp:
         json.dump(mlog, fp, indent=4)
 
 def main():
-
+    """ main entry point
+    """
     parser = argparse.ArgumentParser(
                     prog='slurm_stats',
                     description='Process the json output of slurm\'s sacct command')
